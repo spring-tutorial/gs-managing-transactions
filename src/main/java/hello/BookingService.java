2308f972028b3d03617f6636d499458a166053f0
damiankam <demon.kaminsky@gmail.com>
2016-06-01 14:18:08 +0300

    first commit

---
+++ src/main/java/hello/BookingService.java
@@ -0,0 +1,41 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by dima on 6/1/16.
+ */
+public class BookingService {
+
+    private final static Logger log = LoggerFactory.getLogger(BookingService.class);
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Transactional
+    public void book(String... persons) {
+        for (String person : persons) {
+            log.info("Booking " + person + " in a seat...");
+            jdbcTemplate.update("insert into BOOKINGS(FIRST_NAME) values (?)", person);
+        }
+    };
+
+    public List<String> findAllBookings() {
+        return jdbcTemplate.query("select FIRST_NAME from BOOKINGS", new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return rs.getString("FIRST_NAME");
+            }
+        });
+    }
+
+}
